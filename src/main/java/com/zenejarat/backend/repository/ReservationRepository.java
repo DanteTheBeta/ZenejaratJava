88b58b9c64841abc9343fe7b96fba22aebf633df
DanteTheBeta <streetdog9704@gmail.com>
2025-03-31 16:34:12 +0200

    Update ReservationRepository.java

--- src/main/java/com/zenejarat/backend/repository/ReservationRepository.java
+++ src/main/java/com/zenejarat/backend/repository/ReservationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    // Egyedi lekérdezéseket itt definiálhatsz, ha szükséges
+    
 }
